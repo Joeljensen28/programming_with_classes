ece6903479b2ff5429ac352f42a3266c6f96b317
Joel Jensen <joelsemail28@gmail.com>
2024-05-16 16:13:35 -0600

    removed comment

--- prove/Develop02/Entries.cs
+++ prove/Develop02/Entries.cs
@@ -6,14 +6,6 @@ public class Entry
     public string _promptUsed;
     public string _entryText;
 
-    // public Entry(string prompt, string text, string date)
-    // {
-    //     _promptUsed = prompt;
-    //     _entryText = text;
-    //     _dateResponse = date;
-
-    // }
-
     public void Display()
     {
         Console.WriteLine($"{_dateResponse} {_promptUsed}");
